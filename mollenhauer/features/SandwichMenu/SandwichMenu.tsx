3d5a78bf0cca811c3eb49ad961a7114a57a30250
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2025-03-26 15:21:19 +0300

    feat: make interactive header

--- mollenhauer/features/SandwichMenu/SandwichMenu.tsx
+++ mollenhauer/features/SandwichMenu/SandwichMenu.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import { FC, ReactNode, useState } from "react";
+import cn from "classnames";
 
 import { SandwichMenuList } from "@/features/SandwichMenuList/SandwichMenuList";
 import { SandwichIcon } from "@/components/Icons/SandwichIcon/SandwichIcon";
+
+import { useScroll } from "@/shared/hooks/useScroll";
+import { HEADER_SCROLL } from "@/shared/constants/styles";
 
 import s from "./SandwichMenu.module.scss";
 
 export const SandwichMenu: FC = (): ReactNode => {
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
 
+  const isScrolled = useScroll(HEADER_SCROLL);
+
   const handleMenuClick = () => setIsOpenMenu((prev) => !prev);
 
   return (
     <div className={s.wrap}>
-      <SandwichIcon onClick={handleMenuClick} />
+      <SandwichIcon
+        onClick={handleMenuClick}
+        className={cn({ [s.scrolled]: isScrolled })}
+      />
       <SandwichMenuList isOpen={isOpenMenu} closeMenu={handleMenuClick} />
     </div>
   );
