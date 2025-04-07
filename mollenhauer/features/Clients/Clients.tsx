c9011f0deedce9763215e65a9beba19729f7e876
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2025-04-07 18:27:22 +0300

    feat: add desktop clients

--- mollenhauer/features/Clients/Clients.tsx
+++ mollenhauer/features/Clients/Clients.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import { Swiper as SwiperType } from "swiper/types";
 
 import { H2 } from "@/components/H2/H2";
 
 import { ClientsList } from "@/shared/mocks/content";
 import { ClientCard } from "@/components/ClientCard/ClientCard";
+import { CustomNextPrevNavigation } from "@/components/CustomNextPrevNavigation/CustomNextPrevNavigation";
 
 import s from "./Clients.module.scss";
 import "swiper/scss";
 import "swiper/scss/pagination";
 
 export const Clients: FC = (): ReactNode => {
+  const swiperRef = useRef<SwiperType | null>(null);
+
   return (
     <section className={s.section}>
       <div className={s.description}>
@@ -24,29 +28,39 @@ export const Clients: FC = (): ReactNode => {
         </p>
       </div>
 
-      <Swiper
-        slidesPerView={1}
-        loop
-        pagination={{
-          clickable: true,
-          bulletClass: s.bullet,
-          bulletActiveClass: s.activeBullet,
-          horizontalClass: s.pagination,
-        }}
-        modules={[Pagination]}
-        className={s.swiperWrap}
-				breakpoints={{
-					768: {
-						spaceBetween: 232
-					}
-				}}
-      >
-        {ClientsList.map((el) => (
-          <SwiperSlide key={el.id} className={s.slide}>
-            <ClientCard {...el} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={s.slider}>
+        
+
+        <Swiper
+          slidesPerView={1}
+          loop
+					onSwiper={(swiper: SwiperType) => (swiperRef.current = swiper)}
+          pagination={{
+            clickable: true,
+            bulletClass: s.bullet,
+            bulletActiveClass: s.activeBullet,
+            horizontalClass: s.pagination,
+          }}
+          modules={[Pagination]}
+          className={s.swiperWrap}
+          breakpoints={{
+            768: {
+              spaceBetween: 232,
+            },
+          }}
+        >
+          {ClientsList.map((el) => (
+            <SwiperSlide key={el.id} className={s.slide}>
+              <ClientCard {...el} />
+            </SwiperSlide>
+          ))}
+
+<CustomNextPrevNavigation
+          className={s.prevNextNavigation}
+          swiperRef={swiperRef}
+        />
+        </Swiper>
+      </div>
     </section>
   );
 };
